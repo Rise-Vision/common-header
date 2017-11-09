9d9fb14d926d1b78cf0e2eaddfd7dcdacdd7ec9a
Alex Deaconu <alex.deaconu@gmail.com>
2017-11-09 10:14:56 -0500

    Add missing service

--- dist/js/components/core-api-client.js
+++ dist/js/components/core-api-client.js
@@ -224,6 +224,37 @@ angular.module("risevision.common.app", [
 
 })(angular);
 
+/* jshint evil:true */
+
+/**
+ * Created by rodrigopavezi on 10/16/14.
+ */
+"use strict";
+angular.module("risevision.common.core.endpoint", [
+  "risevision.common.gapi"
+])
+  .factory("callEndpoint", ["coreAPILoader", "$q", "$log",
+    function (coreAPILoader, $q, $log) {
+      return function (method, criteria) {
+        $log.debug("Endpoint called", method, criteria);
+
+        var deferred = $q.defer();
+        coreAPILoader().then(function () {
+          var request = eval(method)(criteria);
+          request.execute(function (resp) {
+            $log.debug("Endpoint resp", resp);
+            if (resp.result) {
+              deferred.resolve(resp);
+            } else {
+              deferred.reject(resp);
+            }
+          });
+        });
+        return deferred.promise;
+      };
+    }
+  ]);
+
 (function (angular) {
   "use strict";
 
