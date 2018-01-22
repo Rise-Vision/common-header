39eb4330fe1dd11e295f7936ffc7a1240a2d4dbf
Alex Deaconu <alex.deaconu@gmail.com>
2018-01-22 15:09:10 -0500

    Google Auth refactoring

--- dist/js/components/userstate.js
+++ dist/js/components/userstate.js
@@ -618,55 +618,8 @@ angular.module("risevision.common.components.logging")
         userState, urlStateService,
         CLIENT_ID, OAUTH2_SCOPES, GOOGLE_OAUTH2_URL) {
 
-        var _accessTokenRefreshHandler = null;
-
-        var _scheduleAccessTokenAutoRefresh = function () {
-          //cancel any existing $interval(s)
-          $interval.cancel(_accessTokenRefreshHandler);
-          _accessTokenRefreshHandler = $interval(function () {
-            //cancel current $interval. It will be re-sheduled if authentication succeeds
-            $interval.cancel(_accessTokenRefreshHandler);
-            //refresh Access Token
-            authenticate();
-          }, 55 * 60 * 1000); //refresh every 55 minutes
-        };
-
-        // TODO: Update
-        var _cancelAccessTokenAutoRefresh = function () {
-          $interval.cancel(_accessTokenRefreshHandler);
-          _accessTokenRefreshHandler = null;
-        };
-
-        var _gapiAuthorize = function (attemptImmediate) {
+        var _gapiAuthorize = function () {
           var deferred = $q.defer();
-
-          var _state = userState._state;
-          var opts = {
-            client_id: CLIENT_ID,
-            scope: OAUTH2_SCOPES,
-            cookie_policy: $location.protocol() + "://" +
-              getBaseDomain()
-          };
-
-          // if (_state.userToken === "dummy") {
-          //   opts.authuser = $http.get(
-          //     "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" +
-          //     _state.params.access_token)
-          //     .then(function (resp) {
-          //       return resp.data.email;
-          //     }, function (err) {
-          //       $log.debug("Error retrieving userinfo", err);
-          //       return opts.authuser;
-          //     });
-          // } else if (_state.userToken) {
-          //   opts.authuser = _state.userToken.email;
-          // }
-
-          if (attemptImmediate) {
-            opts.immediate = true;
-          } else {
-            opts.prompt = "select_account";
-          }
 
           auth2APILoader()
             .then(function (auth2) {
@@ -675,13 +628,6 @@ angular.module("risevision.common.components.logging")
 
               $log.debug("authResult", authResult);
               if (authResult) {
-                if (_state.params) {
-                  // clear token so we don't deal with expiry
-                  delete _state.params;
-                }
-
-                _scheduleAccessTokenAutoRefresh();
-
                 deferred.resolve(authResult);
               } else {
                 deferred.reject("failed to authorize user");
@@ -696,12 +642,12 @@ angular.module("risevision.common.components.logging")
          * Responsible for triggering the Google OAuth process.
          *
          */
-        var authenticate = function (forceAuth) {
+        var authenticate = function () {
           var deferred = $q.defer();
 
           var authResult;
 
-          _gapiAuthorize(!forceAuth)
+          _gapiAuthorize()
             .then(function (res) {
               authResult = res;
 
@@ -723,75 +669,66 @@ angular.module("risevision.common.components.logging")
           return deferred.promise;
         };
 
-        var authenticateRedirect = function (forceAuth) {
+        var _isPopupAuth = function () {
+          return (userState._state.inRVAFrame || ($window.self !== $window.top));
+        };
 
-          if (!forceAuth) {
-            return authenticate(forceAuth);
+        var forceAuthenticate = function () {
+          var loc;
+          var state = $stateParams.state;
+          var redirectUrl;
+
+          // Redirect to full URL path
+          if ($rootScope.redirectToRoot === false) {
+            loc = $window.location.href.substr(0, $window.location.href
+              .indexOf("#")) || $window.location.href;
+
+            state = urlStateService.clearStatePath(state);
           } else {
-            var loc;
-            var state = $stateParams.state;
-            var redirectUrl;
-
-            // Redirect to full URL path
-            if ($rootScope.redirectToRoot === false) {
-              loc = $window.location.href.substr(0, $window.location.href
-                .indexOf("#")) || $window.location.href;
-
-              state = urlStateService.clearStatePath(state);
-            } else {
-              loc = $window.location.origin + "/";
-            }
-
-            userState._state.state = state;
-            userState._persistState();
-            uiFlowManager.persist();
-
-            // redirectUrl = GOOGLE_OAUTH2_URL +
-            //   "?response_type=token" +
-            //   "&scope=" + encodeURIComponent(OAUTH2_SCOPES) +
-            //   "&client_id=" + CLIENT_ID +
-            //   "&redirect_uri=" + encodeURIComponent(loc) +
-            // //http://stackoverflow.com/a/14393492
-            // "&prompt=select_account";
-
-            // if (state) {
-            //   // double encode since response gets decoded once!
-            //   state = encodeURIComponent(state);
-            // 
-            //   redirectUrl += "&state=" + state;
-            // }
-
-            var opts = {
-              // client_id: CLIENT_ID,
-              // scope: OAUTH2_SCOPES,
-              response_type: "token",
-              prompt: "select_account",
-              cookie_policy: $location.protocol() + "://" +
-                getBaseDomain(),
-              ux_mode: "redirect",
-              redirect_uri: loc
-            };
-
-            var deferred = $q.defer();
-
-            auth2APILoader()
-              .then(function (auth2) {
-                return auth2.getAuthInstance().signIn(opts);
-              })
-              .then(null, function (err) {
-                deferred.reject(err);
-              });
-
-            // returns a promise that never get fulfilled since we are redirecting
-            // to that google oauth2 page
-            return deferred.promise;
+            loc = $window.location.origin + "/";
           }
+
+          userState._state.state = state;
+          userState._persistState();
+          uiFlowManager.persist();
+
+          var opts = {
+            response_type: "token",
+            prompt: "select_account",
+            cookie_policy: $location.protocol() + "://" +
+              getBaseDomain(),
+            ux_mode: _isPopupAuth() ? "popup" : "redirect",
+            redirect_uri: loc
+          };
+
+          var deferred = $q.defer();
+
+          auth2APILoader()
+            .then(function (auth2) {
+              return auth2.getAuthInstance().signIn(opts);
+            })
+            .then(function () {
+              if (_isPopupAuth()) {
+                deferred.resolve(authenticate());
+              } else {
+                deferred.resolve();
+              }
+            })
+            .then(null, function (err) {
+              deferred.reject(err);
+            });
+
+          return deferred.promise;
         };
 
         var googleAuthFactory = {
-          authenticate: userState._state.inRVAFrame ||
-            ($window.self !== $window.top) ?
-            authenticate : authenticateRedirect
+          authenticate: function (forceAuth) {
+            if (!forceAuth) {
+              return authenticate();
+            } else {
+              return forceAuthenticate();
+            }
+          }
         };
 
         return googleAuthFactory;
