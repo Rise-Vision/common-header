d8b432f26d5bcd0a40f1200db6ebfc0438f4a75a
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2018-02-20 14:22:46 -0300

    Apply Plan Banner updates (#770)
    * Remove 'type' from plan name
    
    * Reduce template code duplication in plan-banner.html
    
    * Show correct Plan Banner according to current Subscription Status
    
    * Add unit testing
    
    * Use Plan subscription information from getCopyOfSelectedCompany object, instead of fetching it from Subscription Status Service
    
    * Add specific message on Plan Banner when Pro is Subscribed but Plan trial is expired
    
    * Standardize on <strong> for Plan Banner messages
    
    * Remove unused subscriptionStatusService reference

--- dist/js/components/plans.js
+++ dist/js/components/plans.js
@@ -117,28 +117,31 @@ angular.module("risevision.common.components.plans", [
       priceMonth: 0,
       descriptionShort: "Design, distribute and manage your digital signage for free. Unlimited Displays, Companies and Users."
     }, {
+      name: "Basic",
       type: "basic",
       productId: "289",
       pc: "40c092161f547f8f72c9f173cd8eebcb9ca5dd25"
     }, {
+      name: "Advanced",
       type: "advanced",
       productId: "290",
       pc: "93b5595f0d7e4c04a3baba1102ffaecb17607bf4"
     }, {
+      name: "Enterprise",
       type: "enterprise",
       productId: "301",
       pc: "b1844725d63fde197f5125b58b6cba6260ee7a57"
     }, {
+      name: "Enterprise",
       type: "enterprisesub",
       productId: "303",
       pc: "d521f5bfbc1eef109481eebb79831e11c7804ad8"
     }])
     .factory("planFactory", ["$q", "$log", "$rootScope", "$modal", "$templateCache", "userState", "storeAPILoader",
-      "subscriptionStatusService", "currencyService", "PLANS_LIST",
-      function ($q, $log, $rootScope, $modal, $templateCache, userState, storeAPILoader, subscriptionStatusService,
+      "currencyService", "PLANS_LIST",
+      function ($q, $log, $rootScope, $modal, $templateCache, userState, storeAPILoader,
         currencyService, PLANS_LIST) {
         var _factory = {};
-        var _plansCodesList = _.map(PLANS_LIST, "pc");
         var _plansByType = _.keyBy(PLANS_LIST, "type");
         var _plansByCode = _.keyBy(PLANS_LIST, "pc");
 
@@ -175,7 +178,8 @@ angular.module("risevision.common.components.plans", [
                   var priceMap = _.keyBy(plan.pricing, "unit");
                   var price = priceMap[monthKey] || {};
 
-                  plan.type = plan.name.toLowerCase().replace(" plan", "");
+                  plan.name = plan.name.replace(" Plan", "");
+                  plan.type = plan.name.toLowerCase();
                   plan.priceMonth = currency.pickPrice(price.priceUSD, price.priceCAD);
                 });
 
@@ -184,37 +188,6 @@ angular.module("risevision.common.components.plans", [
                 // Add free plan, since it's not returned by the service
                 deferred.resolve([_.cloneDeep(_plansByType.free), planMap.basic, planMap.advanced, planMap.enterprise]);
               });
-            })
-            .catch(function (err) {
-              deferred.reject(err);
-            });
-
-          return deferred.promise;
-        };
-
-        _factory.getCompanyPlan = function (companyId) {
-          $log.debug("getCompanyPlan called.");
-          var deferred = $q.defer();
-
-          subscriptionStatusService.list(_plansCodesList.slice(1), companyId)
-            .then(function (resp) {
-              $log.debug("getCompanyPlan response.", resp);
-
-              // Use Free as default
-              var subscribedPlan = _.cloneDeep(_plansByType.free);
-              var plansMap = _.keyBy(resp, "pc");
-
-              _plansCodesList.forEach(function (planCode) {
-                var plan = plansMap[planCode];
-
-                if (plan && ["Subscribed", "Suspended", "On Trial"].indexOf(plan.status) >= 0) {
-                  subscribedPlan = plan;
-                }
-              });
-
-              subscribedPlan.type = _plansByCode[subscribedPlan.pc].type;
-
-              deferred.resolve(subscribedPlan);
             })
             .catch(function (err) {
               deferred.reject(err);
@@ -239,24 +212,28 @@ angular.module("risevision.common.components.plans", [
         function _getSelectedCurrency() {
           return currencyService()
             .then(function (currency) {
-              var company = userState.getCopyOfUserCompany();
+              var company = userState.getCopyOfSelectedCompany();
               var country = (company && company.country) ? company.country : "";
               return currency.getByCountry(country);
             });
         }
 
         function _loadCurrentPlan() {
-          if (userState.getSelectedCompanyId()) {
-            _factory.getCompanyPlan(userState.getSelectedCompanyId())
-              .then(function (plan) {
-                _factory.currentPlan = plan;
-                $log.debug("Current plan", plan);
-                $rootScope.$emit("risevision.plan.loaded", plan);
-              })
-              .catch(function (err) {
-                $log.debug("Failed to load company's plan", err);
-              });
+          var company = userState.getCopyOfSelectedCompany();
+          var plan = null;
+
+          if (company.id && company.planProductCode) {
+            plan = _.cloneDeep(_plansByCode[company.planProductCode]);
+            plan.status = company.planSubscriptionStatus;
+            plan.trialPeriod = company.planTrialPeriod;
+            plan.proStatus = company.playerProSubscriptionStatus;
+          } else {
+            plan = _.cloneDeep(_plansByType.free);
           }
+
+          _factory.currentPlan = plan;
+          $log.debug("Current plan", plan);
+          $rootScope.$emit("risevision.plan.loaded", plan);
         }
 
         _loadCurrentPlan();
