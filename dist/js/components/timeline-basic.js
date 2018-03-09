24f355d2a878d4c6f90e1b5899933508cf19476d
Francisco Vallarino <fjvallarino@gmail.com>
2018-03-09 18:54:53 -0300

    Provide better disabled state for timeline-basic

--- dist/js/components/timeline-basic.js
+++ dist/js/components/timeline-basic.js
@@ -310,6 +310,6 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('timeline-basic/timeline-textbox.html',
-    '<label class="control-label control-label-secondary u_margin-left"><input ng-model="timeline.always" type="checkbox" ng-disabled="ngDisabled"> Always</label><div id="timelineTextbox" class="panel-editable u_remove-bottom u_clickable" ng-click="openModal()" ng-show="!timeline.always"><div class="label label-tag"><span id="timelineLabel" timeline="timeline">{{timeline.label}}</span></div></div>');
+    '<label class="control-label control-label-secondary u_margin-left"><input ng-model="timeline.always" type="checkbox" ng-disabled="ngDisabled"> Always</label><div id="timelineTextbox" class="panel-editable u_remove-bottom u_clickable" ng-class="{ \'panel-disabled\': ngDisabled }" ng-click="openModal()" ng-show="!timeline.always"><div class="label label-tag"><span id="timelineLabel" timeline="timeline">{{timeline.label}}</span></div></div>');
 }]);
 })();
