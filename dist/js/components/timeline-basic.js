354477afd9ba2c3d30bda2422bb573a78994350d
Francisco Vallarino <fjvallarino@gmail.com>
2018-03-15 07:57:41 -0300

    Move all formating logic into timeline-basic directive

--- dist/js/components/timeline-basic.js
+++ dist/js/components/timeline-basic.js
@@ -210,35 +210,43 @@ angular.module("risevision.common.components.timeline-basic.services")
 (function (angular) {
   "use strict";
   angular.module("risevision.common.components.timeline-basic")
-    .directive("timelineBasicTextbox", ["$modal", "TimelineBasicFactory",
+    .directive("timelineBasicTextbox", ["$modal", "$filter", "TimelineBasicFactory",
       "timelineBasicDescription",
-      function ($modal, TimelineBasicFactory, timelineBasicDescription) {
+      function ($modal, $filter, TimelineBasicFactory, timelineBasicDescription) {
         return {
           restrict: "E",
           scope: {
-            useLocaldate: "=",
-            timeDefined: "=",
-            startTime: "=",
-            endTime: "=",
-            recurrenceDaysOfWeek: "=",
-            latestUpdate: "=",
+            timelineString: "=",
             ngDisabled: "="
           },
           templateUrl: "timeline-basic/timeline-textbox.html",
           link: function ($scope) {
-            // Watch a scope variable that's updated whenever data changes
-            $scope.$watch("latestUpdate", function () {
-              $scope.timeline = TimelineBasicFactory.getTimeline(
-                $scope.useLocaldate,
-                $scope.startTime,
-                $scope.endTime,
-                $scope.recurrenceDaysOfWeek);
+            var updatingExternal = false;
 
-              $scope.timeline.label = timelineBasicDescription.updateLabel($scope.timeline);
+            $scope.monitoringSchedule = {};
+
+            // Watch for the formatted input string
+            $scope.$watch("timelineString", function () {
+              if (!updatingExternal) {
+                $scope.monitoringSchedule = $scope.parseTimeline($scope.timelineString);
+                $scope.timeline = TimelineBasicFactory.getTimeline(
+                  $scope.monitoringSchedule.useLocaldate,
+                  $scope.monitoringSchedule.startTime,
+                  $scope.monitoringSchedule.endTime,
+                  $scope.monitoringSchedule.recurrenceDaysOfWeek);
+
+                $scope.timeline.label = timelineBasicDescription.updateLabel($scope.timeline);
+              } else {
+                updatingExternal = false;
+              }
             });
 
             $scope.$watch("timeline.always", function (newValue) {
-              $scope.timeDefined = !newValue;
+              if (!updatingExternal) {
+                $scope.monitoringSchedule.timeDefined = !newValue;
+                updatingExternal = true;
+                $scope.timelineString = $scope.formatTimeline($scope.monitoringSchedule);
+              }
             });
 
             $scope.openModal = function () {
@@ -258,21 +266,84 @@ angular.module("risevision.common.components.timeline-basic.services")
               });
 
               modalInstance.result.then(function (timeline) {
-                //do what you need if user presses ok
-                $scope.timeline = timeline;
+                updatingExternal = true;
 
-                $scope.timeDefined = !(timeline.allDay && timeline.everyDay);
-                $scope.startTime = timeline.startTime;
-                $scope.endTime = timeline.endTime;
-                $scope.recurrenceDaysOfWeek = timeline.recurrenceDaysOfWeek;
-                $scope.latestUpdate = Date.now();
+                $scope.timeline = timeline;
+                $scope.timeline.always = (timeline.allDay && timeline.everyDay);
+                $scope.monitoringSchedule.timeDefined = !$scope.timeline.always;
+                $scope.monitoringSchedule.startTime = timeline.startTime;
+                $scope.monitoringSchedule.endTime = timeline.endTime;
+                $scope.monitoringSchedule.recurrenceDaysOfWeek = timeline.recurrenceDaysOfWeek;
 
                 $scope.timeline.label = timelineBasicDescription.updateLabel($scope.timeline);
+
+                $scope.timelineString = $scope.formatTimeline($scope.monitoringSchedule);
               }, function () {
                 // do what you need to do if user cancels
               });
             };
-          }
+
+            $scope.formatTimeline = function (timeline) {
+              var resp = {};
+
+              if (!timeline || !timeline.timeDefined) {
+                return null;
+              }
+
+              if (timeline.startTime || timeline.endTime) {
+                resp.time = {};
+                resp.time.start = timeline.startTime ? $filter("date")(new Date(timeline.startTime), "HH:mm") :
+                  null;
+                resp.time.end = timeline.endTime ? $filter("date")(new Date(timeline.endTime), "HH:mm") : null;
+              }
+
+              if (timeline.recurrenceDaysOfWeek && timeline.recurrenceDaysOfWeek.length > 0) {
+                resp.week = timeline.recurrenceDaysOfWeek.map(function (day) {
+                  return {
+                    day: day,
+                    active: true
+                  };
+                });
+              }
+
+              resp = JSON.stringify(resp);
+
+              return resp !== "{}" ? resp : null;
+            };
+
+            $scope.parseTimeline = function (tl) {
+              var timeline = {};
+
+              if (tl && tl !== "{}") {
+                tl = JSON.parse(tl);
+
+                timeline.timeDefined = true;
+
+                if (tl.time) {
+                  timeline.startTime = tl.time.start ? $scope.reformatTime(tl.time.start) : null;
+                  timeline.endTime = tl.time.end ? $scope.reformatTime(tl.time.end) : null;
+                }
+
+                if (tl.week) {
+                  timeline.recurrenceDaysOfWeek = [];
+
+                  tl.week.forEach(function (d) {
+                    if (d.active) {
+                      timeline.recurrenceDaysOfWeek.push(d.day);
+                    }
+                  });
+                }
+              }
+
+              return timeline;
+            };
+
+            $scope.reformatTime = function (timeString) {
+              var today = $filter("date")(new Date(), "dd-MMM-yyyy");
+              var fullDate = new Date(today + " " + timeString);
+              return $filter("date")(fullDate, "dd-MMM-yyyy hh:mm a");
+            };
+          } // link
         };
       }
     ]);
