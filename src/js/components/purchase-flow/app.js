267073f75efc4c4bcb4d5db92d59fd4340509c8a
Alex Deaconu <alex.deaconu@risevision.com>
2018-08-08 09:29:45 -0400

    Load Stripe js and create CC token (#855)
    Load stripe (requires script tag in html)
    Set key conditionally on environment
    
    Check card fields with Card API
    Create token via Stripe API

--- src/js/components/purchase-flow/app.js
+++ src/js/components/purchase-flow/app.js
@@ -2,6 +2,7 @@
 
 angular.module("risevision.common.components.purchase-flow", [
   "risevision.store.authorization",
+  "risevision.common.config",
   "risevision.common.gapi",
   "risevision.common.geodata",
   "risevision.common.currency",
