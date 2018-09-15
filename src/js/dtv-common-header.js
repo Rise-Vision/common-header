96f37031862e4a68b6a03925e4c54f433e22f44d
Francisco Vallarino <fjvallarino@gmail.com>
2018-09-15 01:13:45 -0300

    Redirect Chargebee users to Apps when trying to access Store

--- src/js/dtv-common-header.js
+++ src/js/dtv-common-header.js
@@ -133,8 +133,10 @@ angular.module("risevision.common.header", [
 
 .run(["segmentAnalytics", "SEGMENT_API_KEY", "ENABLE_INTERCOM_MESSAGING",
   "analyticsEvents", "$document",
+  "$rootScope", "$window", "userState", "STORE_URL", "APPS_URL",
   function (segmentAnalytics, SEGMENT_API_KEY, ENABLE_INTERCOM_MESSAGING,
-    analyticsEvents, $document) {
+    analyticsEvents, $document,
+    $rootScope, $window, userState, STORE_URL, APPS_URL) {
     analyticsEvents.initialize();
     segmentAnalytics.load(SEGMENT_API_KEY, ENABLE_INTERCOM_MESSAGING);
 
@@ -162,6 +164,17 @@ angular.module("risevision.common.header", [
       }
       if (doPrevent) {
         event.preventDefault();
+      }
+    });
+
+    $rootScope.$on("risevision.company.selectedCompanyChanged", function () {
+      // Redirect Store requests by Chargebee users to Apps
+      var currentURL = $window.location.href;
+      var isStoreRequest = currentURL.indexOf(STORE_URL) >= 0;
+      var isInvoicesRequest = currentURL.indexOf(STORE_URL + "account/view/invoicesHistory") >= 0;
+
+      if (userState.isSelectedCompanyChargebee() && isStoreRequest && !isInvoicesRequest) {
+        $window.location.href = APPS_URL + "?cid=" + userState.getSelectedCompanyId();
       }
     });
   }
