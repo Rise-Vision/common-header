630cc020b3e525d89bce93a5ab481117f97ae821
Oleg Khasimkhanov <olegkhasimkhanov@Olegs-MacBook-Pro.local>
2019-06-18 13:42:42 -0400

    Fix for issue 1015 - Cancelled subscription shows "Managed by parent" for subcompany

--- dist/js/components/plans.js
+++ dist/js/components/plans.js
@@ -131,6 +131,8 @@ angular.module("risevision.common.components.plans", [
             plan = _.cloneDeep(_plansByType.free);
           }
 
+          _factory.currentPlan = plan;
+
           plan.playerProTotalLicenseCount = company.playerProTotalLicenseCount;
           plan.playerProAvailableLicenseCount = company.playerProAvailableLicenseCount;
 
@@ -141,11 +143,10 @@ angular.module("risevision.common.components.plans", [
           }
 
           plan.isPurchasedByParent = !!company.planBillToId && !!company.planShipToId && (company.planBillToId !==
-            company.planShipToId);
+            company.planShipToId) && (_factory.isSubscribed() || _factory.isCancelledActive());
           plan.parentPlanCompanyName = company.parentPlanCompanyName;
           plan.parentPlanContactEmail = company.parentPlanContactEmail;
 
-          _factory.currentPlan = plan;
           $log.debug("Current plan", plan);
           $rootScope.$emit("risevision.plan.loaded", plan);
         };
@@ -174,7 +175,7 @@ angular.module("risevision.common.components.plans", [
         };
 
         _factory.isParentPlan = function () {
-          return !!_factory.currentPlan.parentPlan;
+          return !!_factory.currentPlan.parentPlan && (_factory.isSubscribed() || _factory.isCancelledActive());
         };
 
         _factory.isEnterpriseSubCompany = function () {
--- src/js/components/plans/svc-current-plan-factory.js
+++ src/js/components/plans/svc-current-plan-factory.js
@@ -23,6 +23,8 @@
             plan = _.cloneDeep(_plansByType.free);
           }
 
+          _factory.currentPlan = plan;
+
           plan.playerProTotalLicenseCount = company.playerProTotalLicenseCount;
           plan.playerProAvailableLicenseCount = company.playerProAvailableLicenseCount;
 
@@ -33,11 +35,10 @@
           }
 
           plan.isPurchasedByParent = !!company.planBillToId && !!company.planShipToId && (company.planBillToId !==
-            company.planShipToId);
+            company.planShipToId) && (_factory.isSubscribed() || _factory.isCancelledActive());
           plan.parentPlanCompanyName = company.parentPlanCompanyName;
           plan.parentPlanContactEmail = company.parentPlanContactEmail;
 
-          _factory.currentPlan = plan;
           $log.debug("Current plan", plan);
           $rootScope.$emit("risevision.plan.loaded", plan);
         };
@@ -66,7 +67,7 @@
         };
 
         _factory.isParentPlan = function () {
-          return !!_factory.currentPlan.parentPlan;
+          return !!_factory.currentPlan.parentPlan && (_factory.isSubscribed() || _factory.isCancelledActive());
         };
 
         _factory.isEnterpriseSubCompany = function () {
